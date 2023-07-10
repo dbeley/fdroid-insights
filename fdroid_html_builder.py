5f1217cd2f3196d90b08fd665a3c00868e539ee1
dbeley <6568955+dbeley@users.noreply.github.com>
2023-07-10 21:12:06 +0200

    Fix html structure, some refacto

--- fdroid_html_builder.py
+++ fdroid_html_builder.py
@@ -27,33 +27,29 @@ df = df.fillna(
 )
 
 header = (
-    "<th>Name</th>"
-    "<th>Repository</th>"
-    "<th>Repository Stars Count</th>"
-    "<th>Repository Forks Count</th>"
-    "<th>Repository Domain</th>"
-    "<th>Summary</th>"
-    "<th>Categories</th>"
-    "<th>Added</th>"
-    "<th>Last Updated</th>"
+    "<thead>\n"
+    "<tr>\n"
+    "<th>Name</th>\n"
+    "<th>Repository</th>\n"
+    "<th>Repository Stars Count</th>\n"
+    "<th>Repository Forks Count</th>\n"
+    "<th>Repository Domain</th>\n"
+    "<th>Summary</th>\n"
+    "<th>Categories</th>\n"
+    "<th>Added</th>\n"
+    "<th>Last Updated</th>\n"
+    "</tr>\n"
+    "</thead>\n"
 )
 
-table_data = ""
-table_data += "<tbody>\n"
+table_data = "<tbody>\n"
 for index, row in df.iterrows():
-    repository = row["repository"]
-    icon = row["icon"]
-    url = row["url"]
-    name_with_icon = (
-        # f"<td><img src='{icon}' width='32' alt='{icon}'> <a href='{url}'>{row['name']}</a></td>"
-        # if icon
-        f"<td><a href='{url}'>{row['name']}</a></td>"
-    )
+    name = f"<td><a href='{row['url']}'>{row['name']}</a></td>"
     table_data += (
         "<tr>\n"
-        f"{name_with_icon}"
+        f"{name}"
         "\n"
-        f"<td><a href='{repository}'>{repository}</a></td>"
+        f"<td><a href='{row['repository']}'>{row['repository']}</a></td>"
         "\n"
         f"<td>{row['repository_stars_count']}</td>"
         "\n"
@@ -71,6 +67,7 @@ for index, row in df.iterrows():
         "\n"
         "</tr>\n"
     )
+table_data += "</tbody>\n"
 
 date_update = datetime.today().strftime("%Y-%m-%d")
 

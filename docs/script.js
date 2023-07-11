f83b53c531a9b272f96cd7334d4e9c538e858c0c
dbeley <6568955+dbeley@users.noreply.github.com>
2023-07-11 08:56:52 +0200

    Add scroll to top behavior

--- docs/script.js
+++ docs/script.js
@@ -21,6 +21,7 @@ $(document).ready(function() {
 			"autoWidth": true,
 			"aLengthMenu": [[10, 25, 50, 100, 1000, -1], [10, 25, 50, 100, 1000, "All"]],
 			"pageLength": 25,
+            "scrollToTop": true,
 			"order": [[8, 'desc']],
     		"columnDefs": [
       	  	  { type: numbersType, targets: [2, 3] },
